353e39c1cc6e3565f3c27bed2dd8fbd41811a5d4
Max Wofford <max@maxwofford.com>
2026-03-16 11:29:04 -0400

    Smarter field callbacks

--- lib/ferret/searchable.rb
+++ lib/ferret/searchable.rb
@@ -8,14 +8,22 @@ module Ferret
 
     class_methods do
       def has_ferret_search(*fields)
-        Ferret.registry[self] = fields.map(&:to_sym)
+        require "set"
+        ferret_fields = fields.map(&:to_sym).to_set.freeze
+        Ferret.registry[self] = ferret_fields
 
         # Store the base class name for STI support — subclasses should
         # all index under the parent class that called has_ferret_search.
         ferret_base_name = name
 
-        after_commit :ferret_index, on: %i[create update], if: -> { Ferret.configuration.embed_on_save }
+        after_commit :ferret_index, on: %i[create update],
+                     if: -> { Ferret.configuration.embed_on_save && saved_changes_to_ferret_fields? }
         after_commit :ferret_remove, on: :destroy
+
+        define_method(:saved_changes_to_ferret_fields?) do
+          # Always index on create; on update, only if a searchable field changed
+          !persisted_before_last_save || saved_changes.keys.any? { |k| ferret_fields.include?(k.to_sym) }
+        end
 
         define_method(:ferret_index) do
           if defined?(Ferret::EmbedRecordJob)
